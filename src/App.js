b753833a137952dcebdd04040aca1a9d4103566f
thiagorasan <trs.tiba@gmail.com>
2021-04-24 07:44:25 -0300

    iniciando projeto dogs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import Api from './api/Api';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import './App.css';
 
 function App() {
   return (
-    <div><Api /> </div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Routes path="/" element={<Home />} />
+          <Routes path="/" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
